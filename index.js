2ebd97bd5498d31c7fa40b63eb18e96959c75e14
mdansarulislam641 <mdansarulislam641.com>
2022-11-24 18:57:17 +0600

    add product api done

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fg2t6rb.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+// db collection
 const categoryCollection = client.db('laptop-resell-market').collection('category');
+const productsCollection = client.db('laptop-resell-market').collection('products')
 function run() {
     try {
         client.connect();
@@ -29,6 +31,22 @@ app.get('/categories', async (req, res) => {
         const query = {};
         const result = await categoryCollection.find(query).toArray();
         res.send(result)
+    }
+    catch(error){
+        res.send({
+            success:false,
+            message:error.message
+        })
+    }
+
+})
+
+// add product seller product add for api
+app.post('/add-product',async(req, res)=>{
+    try{
+        const productInfo = req.body;
+        const result = await productsCollection.insertOne(productInfo);
+        res.send(result);
     }
     catch(error){
         res.send({
